90f784d79b06a7a0d40cbc2532bde16f725f0312
a1633661 <oscarsalem.garcia@student.adelaide.edu.au>
2018-01-22 22:54:27 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const hbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+const app = express();
+
+/////############ HBS SETUP ###############///
+app.engine('hbs', hbs({
+    extname:'hbs',
+    defaultLayout: 'layout',
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials'
+}));
+
+app.set('view engine', 'hbs');
+
+//CSS
+app.use("/css", express.static(__dirname + '/public/css'))
+
+const jsonParser = bodyParser.json(); 
+
+//GET
+app.get('/add_note', (req,res)=>{
+  res.render('add_note');
+});
+
+
+app.get('/', (req,res)=>{
+
+    fetch('http://localhost:3004/messages')
+    .then(response=>{
+        response.json().then(json =>{
+            res.render('home',{
+                articles: json
+            });
+        })
+    })
+    .catch(error =>{
+        console.log(error);
+    })
+
+
+  
+});
+
+//POST
+app.post('/api/add_note', jsonParser,(req,res)=>{
+  
+    fetch('http://localhost:3004/messages',{
+        method: 'POST',
+        body: JSON.stringify(req.body),
+        headers:{
+            'Content-Type': 'application/json'
+        }
+    }).then(response=>{
+        res.status(200).send();
+    })
+
+});
+
+
+
+/////############ PORT ###############///
+
+const port = process.env.PORT || 3000;
+app.listen(port,()=>{
+    console.log(`Server up on port ${port}`);
+})
